b4277021c87dd41e12ad4deb42abe18d6545f82c
dhannn52 <122761757+dhannn52@users.noreply.github.com>
2023-02-28 15:02:35 +0700

    Add files via upload

---
+++ paket.py
@@ -0,0 +1,14 @@
+from pkg.modul1 import *
+from pkg.modul2 import *
+from pkg.hitungnilai import hitung_nilai
+
+def main():
+
+        f1()
+        f2()
+
+        f3()
+        f4()
+        print("Nilai Akhir : ",hitung_nilai(80,90))
+main()
+
